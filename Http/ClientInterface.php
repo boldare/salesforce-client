5fcab04773c329fd5953d3436628800bb3580467
Adrian Waler <adrian.waler@xsolve.pl>
2017-01-05 11:27:32 +0100

    Fix - docblock for ClientInterface

--- Http/ClientInterface.php
+++ Http/ClientInterface.php
@@ -7,10 +7,6 @@ use Psr\Http\Message\ResponseInterface;
 interface ClientInterface
 {
     /**
-     * @param string $method
-     * @param string $uri
-     * @param array $options
-     *
      * @throws HttpException
      */
     public function request(string $method, string $uri = '', array $options = []) : ResponseInterface;
