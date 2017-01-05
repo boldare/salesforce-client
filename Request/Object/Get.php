45b38bdccea19f503490ef0bc28421009d6b4dac
Kryniol <pawel.krynicki@xsolve.pl>
2017-01-05 11:08:37 +0100

    #9 decoupling Get and Update request classes

--- Request/Object/Get.php
+++ Request/Object/Get.php
@@ -2,8 +2,47 @@
 
 namespace Xsolve\SalesforceClient\Request\Object;
 
-class Get extends Update
+use Xsolve\SalesforceClient\Request\SalesforceRequestInterface;
+
+class Get implements SalesforceRequestInterface
 {
+    const ENDPOINT = '/sobjects/%s/%s/';
+
+    /**
+     * @var string
+     */
+    protected $objectType;
+
+    /**
+     * @var string
+     */
+    protected $id;
+
+    /**
+     * @var array
+     */
+    protected $params;
+
+    /**
+     * @param string $objectType
+     * @param string $id
+     * @param array $params
+     */
+    public function __construct(string $objectType, string $id, array $params = [])
+    {
+        $this->objectType = $objectType;
+        $this->id = $id;
+        $this->params = $params;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getEndpoint(): string
+    {
+        return sprintf(self::ENDPOINT, $this->objectType, $this->id);
+    }
+
     /**
      * {@inheritdoc}
      */
