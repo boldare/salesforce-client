32a78e7c981822f4a30610ff0fdbf6df7856bc6e
Adrian Waler <adrian.waler@xsolve.pl>
2017-01-09 14:58:54 +0100

    Contract and Order

--- src/Enum/AbstractSObjectType.php
+++ src/Enum/AbstractSObjectType.php
@@ -9,4 +9,6 @@ abstract class AbstractSObjectType extends AbstractEnumeration
     const LEAD = 'Lead';
     const ACCOUNT = 'Account';
     const PRODUCT = 'Product2';
+    const CONTRACT = 'Contract';
+    const ORDER = 'Order';
 }
